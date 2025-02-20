54b5041c5203a8fd16547d356668c50abcbd05fa
Hasib Hasan <mohammodhasibhasan@gmail.com>
2025-02-20 10:09:27 +0600

    practice Problem 1

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 // ============== practice Problem 👉👉 1 ==============
+
 let x = 20;
 let y = 40;
 console.log(x+y);
+
+
+// ============== practice Problem 👉👉 2 ==============
+
+let a = 58;
+let b = 19;
+console.log(a-b)
